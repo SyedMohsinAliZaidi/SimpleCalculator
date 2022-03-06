8aa41bf28d08be7922926d5e80ec456e70bfc791
Mohsinali <Mohsinali@DESKTOP-BH3TJ4D>
2022-03-06 22:19:52 +0500

    Add project files.

---
+++ Calculate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    public static class Calculate
+    {
+        public static double DoCalculation(double val1, double val2, string operatorMath)
+        {
+            double result = 0;
+
+            switch (operatorMath)
+            {
+                case "/":
+                    result = val1 / val2;
+                    break;
+                case "-":
+                    result = val1 - val2;
+                    break;
+                case "*":
+                    result = val1 * val2;
+                    break;
+                case "+":
+                    result = val1 + val2;
+                    break;
+                default:
+                    break;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,83 @@
+﻿namespace SimpleCalculator;
+
+public partial class MainPage : ContentPage
+{
+	int currentState = 1;
+	string operatorMath;
+	double firstNum, secondNum;
+
+	public MainPage()
+	{
+		InitializeComponent();
+		OnClear(this, null);
+	}
+
+	void OnClear(object sender, EventArgs e) 
+	{ 
+		firstNum = 0;
+		secondNum = 0;
+		currentState = 1;
+		this.result.Text = "0";
+	}
+
+	void OnSquareRoot(object sender,EventArgs e)
+    {
+		if (firstNum == 0)
+			return;
+		firstNum = firstNum * firstNum;
+		this.result.Text = firstNum.ToString();
+    }
+
+	void OnNumberSelection(object sender,EventArgs e)
+    {
+		Button button = (Button)sender;
+		string btnPressed = button.Text;
+
+		if(this.result.Text == "0" || currentState < 0)
+        {
+			this.result.Text = string.Empty;
+			if(currentState < 0)
+				currentState *= -1;
+        }
+
+		this.result.Text += btnPressed;
+
+		double number;
+		if(double.TryParse(this.result.Text,out number))
+        {
+			this.result.Text = number.ToString("N0");
+			if(currentState == 1)
+            {
+				firstNum = number;
+            }
+            else
+            {
+				secondNum = number;
+            }
+        }
+    }
+
+
+	void onOperatorSelection(object sender ,EventArgs e)
+    {
+		currentState = -2;
+		Button button= (Button)sender;
+		string btnPressed = button.Text;
+		operatorMath = btnPressed;
+    }
+
+	void onCalculate(object sender,EventArgs e)
+    {
+		if(currentState == 2)
+        {
+			var result = Calculate.DoCalculation(firstNum, secondNum, operatorMath);
+
+			this.result.Text = result.ToString();
+			firstNum = result;
+			currentState = -1;
+        }
+    }
+
+
+}
+
